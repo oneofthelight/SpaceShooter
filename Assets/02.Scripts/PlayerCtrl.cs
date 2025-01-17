9ff25dc621235bbf32512ce49e1de587e24a4032
oneofthelight <130127365+oneofthelight@users.noreply.github.com>
2025-01-17 14:48:31 +0900

    PlayCtrl

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
+    int gv = 0;
+    void Awake()
+    {
+        gv = 1;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        gv = 100;
     }
 
     // Update is called once per frame
