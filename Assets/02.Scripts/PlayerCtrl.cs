962e0c640f15c3352e4b87360ca4f0075b99cbea
oneofthelight <130127365+oneofthelight@users.noreply.github.com>
2025-01-22 15:48:54 +0900

    캐릭터 이동

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -6,7 +6,10 @@ using UnityEngine;
 public class PlayerCtrl : MonoBehaviour
 {
     private Transform tr;
+    // 이동 속도 변수
     public float moveSpeed = 10.0f; 
+    
+    public float turnSpeed = 80.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +22,10 @@ public class PlayerCtrl : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        float r = Input.GetAxis("Mouse X");
 
-        Debug.Log("h=" + h);
-        Debug.Log("v=" + v);
+        // Debug.Log("h=" + h);
+        // Debug.Log("v=" + v);
 
         // Transform 컴포넌트의 position 속성값을 변경
         //transform.position += new Vector3(0, 0, 1);
@@ -29,6 +33,11 @@ public class PlayerCtrl : MonoBehaviour
         // 정규화 벡터를 사용한 코드
         //tr.position += Vector3.forward * 1;
 
-        tr.Translate(Vector3.forward * Time.deltaTime * v * moveSpeed);
+
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+
+        tr.Translate(moveDir.normalized * Time.deltaTime  * moveSpeed);
+        
+        tr.Rotate(Vector3.up * turnSpeed * Time.deltaTime * r);
     }
 }
