883a4d46975ad7851ece8f4381a5704f390e47d6
oneofthelight <130127365+oneofthelight@users.noreply.github.com>
2025-02-04 15:49:37 +0900

    Shoot

---
+++ Assets/02.Scripts/FireCtrl.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCtrl : MonoBehaviour
+{
+    // 총알 프리펩
+    public GameObject bullet;
+    // 총알 발사 좌표
+    public Transform firePos;
+    void Update()
+    {
+        // 마우스 왼쪽 버튼을 클릭 했을 때 Fire 함수 호출
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        } 
+    }
+    void Fire()
+    {   
+        Instantiate(bullet, firePos.position, firePos.rotation);
+    }
+
+}
