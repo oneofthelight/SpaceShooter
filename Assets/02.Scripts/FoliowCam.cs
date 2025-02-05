5c0f34188f9e40ef566dedc7068afc5d998fe862
oneofthelight <130127365+oneofthelight@users.noreply.github.com>
2025-02-05 15:49:21 +0900

    총알 궤적

--- Assets/02.Scripts/FoliowCam.cs
+++ Assets/02.Scripts/FoliowCam.cs
@@ -11,8 +11,13 @@ public class FoliowCam : MonoBehaviour
     // 대상으로부터 거리
     [Range(2.0f, 20.0f)]
     public float distance = 10.0f;
+    // y축 높이
     [Range(0.0f, 10.0f)]
     public float height = 2.0f;
+    // 반응 속도
+    public float damping = 10.0f;
+    public float targetTrOffset = 2.0f; 
+    private Vector3 velocity = Vector3.zero;
     void Start()
     {   
         camTr = GetComponent<Transform>();
@@ -22,10 +27,16 @@ public class FoliowCam : MonoBehaviour
     void LateUpdate()
     {
         //추적할 대상 뒤쪽으로 distance만큼, 높이는 height만큼 이동
-        camTr.position = targetTr.position
+        Vector3 pos = targetTr.position
                         + (-targetTr.forward * distance)
                         + (Vector3.up * height);
+
+        camTr.position = Vector3.SmoothDamp(camTr.position,
+                                            pos,
+                                            ref velocity,
+                                            damping);
+                                            
         // 피벗좌표를 향해 회전
-        camTr.LookAt(targetTr.position);
+        camTr.LookAt(targetTr.position + (targetTr.up * targetTrOffset));
     }
 }
