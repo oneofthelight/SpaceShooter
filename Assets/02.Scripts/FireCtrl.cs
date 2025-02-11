f8867196504e48f9026f26782da08d6521de72ff
oneofthelight <130127365+oneofthelight@users.noreply.github.com>
2025-02-11 15:50:24 +0900

    총소리 입력

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -2,12 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 반드시 필요한 컴포넌트를 삭제 안되도록 하는 어트리뷰트
+[RequireComponent(typeof(AudioSource))]
 public class FireCtrl : MonoBehaviour
 {
     // 총알 프리펩
     public GameObject bullet;
     // 총알 발사 좌표
     public Transform firePos;
+    // 총소리 음원
+    public AudioClip fireSfx;
+
+    // 오디오 컴포넌트
+    private new AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
     void Update()
     {
         // 마우스 왼쪽 버튼을 클릭 했을 때 Fire 함수 호출
@@ -19,6 +31,8 @@ public class FireCtrl : MonoBehaviour
     void Fire()
     {   
         Instantiate(bullet, firePos.position, firePos.rotation);
+        // 총소리 발생
+        audio.PlayOneShot(fireSfx, 1.0f);
     }
 
 }
